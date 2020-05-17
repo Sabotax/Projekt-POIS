dd25925fce6112c4d1a3cbb9adf957c84aa76688
Sabotax <62028372+Sabotax@users.noreply.github.com>
2020-05-17 21:35:24 +0200

    gey
    copy from lab9, zrobienie własnej tekstury i jej test

---
+++ projekt/main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+std::vector<std::unique_ptr<sf::Drawable>> create_shapes()
+{
+    std::vector<std::unique_ptr<sf::Drawable>> shapes;
+
+    std::unique_ptr<sf::CircleShape> circle = std::make_unique<sf::CircleShape>(100.0);
+    circle->setPosition(100.0, 300.0);
+    circle->setFillColor(sf::Color(100, 250, 50));
+    shapes.emplace_back(std::move(circle));
+
+    std::unique_ptr<sf::RectangleShape> rectangle = std::make_unique<sf::RectangleShape>(sf::Vector2f(120.0, 60.0));
+    rectangle->setPosition(500.0, 400.0);
+    rectangle->setFillColor(sf::Color(100, 50, 250));
+    shapes.emplace_back(std::move(rectangle));
+
+    std::unique_ptr<sf::ConvexShape> triangle = std::make_unique<sf::ConvexShape>();
+    triangle->setPointCount(3);
+    triangle->setPoint(0, sf::Vector2f(0.0, 0.0));
+    triangle->setPoint(1, sf::Vector2f(0.0, 100.0));
+    triangle->setPoint(2, sf::Vector2f(140.0, 40.0));
+    triangle->setOutlineColor(sf::Color::Red);
+    triangle->setOutlineThickness(5);
+    triangle->setPosition(600.0, 100.0);
+    shapes.emplace_back(std::move(triangle));
+
+    return shapes;
+}
+
+int main()
+{
+
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    std::vector<std::unique_ptr <sf::Drawable> > shapes = create_shapes();
+
+    sf::Texture texture_grass;
+    if (!texture_grass.loadFromFile("../textures/grass.png")) {
+        std::cerr << "Could not load texture" << std::endl;
+        return 1;
+    }
+    texture_grass.setRepeated(true);
+
+    sf::Sprite grass;
+    grass.setTexture(texture_grass);
+    double skala = 0.3;
+    grass.setScale(skala, skala);
+    grass.setTextureRect(sf::IntRect(0, 0, window.getSize().x*1/skala, window.getSize().y*1/skala));
+    grass.setPosition(0,0);
+
+    sf::Texture texture_gey;
+    if(!texture_gey.loadFromFile("../textures/gey.png")) { return 1; }
+
+    sf::Sprite gey;
+    gey.setTexture(texture_gey);
+    //guy.setTextureRect(sf::IntRect(10, 20, 20, 15)); //left, top, width, height
+
+    gey.setPosition(100,100);
+
+
+    sf::Texture texture_wall;
+    if(!texture_wall.loadFromFile("../textures/wall.png")) { return 1; }
+    texture_wall.setRepeated(true);
+
+    sf::Sprite wall;
+    wall.setTexture(texture_wall);
+    wall.setScale(0.3, 0.3);
+    wall.setTextureRect(sf::IntRect(0, 0, 500, 500));
+    wall.setPosition(0,0);
+
+
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        //control panel
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            gey.move(0,-1);
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            gey.move(0,1);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            gey.move(-1,0);
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            gey.move(1,0);
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        for(const auto &x: shapes) window.draw(*x);
+
+        window.draw(grass);
+        window.draw(gey);
+
+
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+
+}
