845c489e37489ddff164809ac3e407fce04bad9a
Sabotax <62028372+Sabotax@users.noreply.github.com>
2020-05-19 16:37:12 +0200

    map
    nie dziala

--- projekt/Boot.cpp
+++ projekt/Boot.cpp
@@ -1,22 +1,29 @@
 #include "Header.h"
 
 namespace Boot {
-    //static std::vector<sf::Texture> tekstury;
     void LoadTextures() {
-        sf::Texture texture_grass;
-        if (!texture_grass.loadFromFile("../textures/grass.png")) {
-            std::cerr << "Could not load texture" << std::endl;
+        tekstury.emplace("grass", LoadOneTexture("../textures/grass.png"));
+        tekstury.emplace("wall", LoadOneTexture("../textures/wall.png"));
+    }
+    sf::Texture LoadOneTexture(std::string path) {
+        sf::Texture texture;
+        if (!texture.loadFromFile(path)) {
+            std::cerr << "Could not load texture at " << path << std::endl;
         }
-
-        tekstury.emplace_back(texture_grass);
+        return texture;
     }
     void Boot(std::unique_ptr< sf::RenderWindow > window) {
         LoadTextures();
-        sf::Sprite grass;
-        grass.setTexture(tekstury[0]);
-        double skala = 0.3;
-        grass.setScale(skala, skala);
-        grass.setTextureRect(sf::IntRect(0, 0, window->getSize().x*1/skala, window->getSize().y*1/skala));
-        grass.setPosition(0,0);
+
+//        sf::Sprite grass;
+//        grass.setTexture(tekstury["grass"]);
+//        double skala = 0.3;
+//        grass.setScale(skala, skala);
+//        grass.setTextureRect(sf::IntRect(0, 0, window->getSize().x*1/skala, window->getSize().y*1/skala));
+//        grass.setPosition(0,0);
+
+    }
+    void Boot() {
+        LoadTextures();
     }
 };
--- projekt/Header.h
+++ projekt/Header.h
@@ -5,12 +5,15 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <map>
 //klasy
 class Tile;
 namespace Boot {
-    static std::vector<sf::Texture> tekstury;
+    static std::map< std::string , sf::Texture > tekstury;
     void LoadTextures();
+    sf::Texture LoadOneTexture(std::string path);
     void Boot(std::unique_ptr< sf::RenderWindow > window);
+    void Boot();
 }
 
 #endif // HEADER_H
