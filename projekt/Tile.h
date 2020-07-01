0565fcf8342b9c0471199d412473b563bb1c8d0b
Sabotax <62028372+Sabotax@users.noreply.github.com>
2020-07-01 21:34:52 +0200

    proby naprawy bugow (bezowocne)

--- projekt/Tile.h
+++ projekt/Tile.h
@@ -33,6 +33,7 @@ public:
     static int wysokosc_okna;
     bool czy_zajete = false;
     static std::shared_ptr< sf::RenderWindow > window;
+    bool czy_koordy_wchodza_w_tile(sf::Vector2i coords);
 };
 
 #endif // TILE_H
